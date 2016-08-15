a797f3c0cc2453f03f3d65b560c64840fd9264f5
yofreke <sparkyjoe94@aim.com>
2016-08-15 13:29:49 -0700

    transform from

--- es6-imports.js
+++ es6-imports.js
@@ -1,5 +1,7 @@
 'use strict';
-const jsio = require('jsio');
+const named = require('named-regexp').named;
+const _ = require('lodash');
+
 
 // Stolen from: https://github.com/gameclosure/js.io/blob/bf8cdfa2c19fd610b179ce47ca7101f36988c7e9/packages/preprocessors/import.js //
 var importExpr = /^(\s*)(import\s+[^=+*"'\r\n;\/]+|from\s+[^=+"'\r\n;\/ ]+\s+import\s+[^=+"'\r\n;\/]+)(;|\/|$)/gm;
@@ -12,11 +14,84 @@ var replaceFn = function (raw, p1, p2, p3) {
 // ---- //
 
 
+// const jsio = require('jsio');
 // Prime jsio with some common paths
+// jsio.addPath('./timestep/src');
 // path: jsio/packages . lib timestep/src
 // pathCache: devkit -> devkit-core/src/clientapi
 // pathCache: squill -> squill/
 
+
+const impName = name => '(:<' + name + '>[a-zA-Z\\.\\$]+?)';
+const importPatterns = {
+  single: {
+    re: named(new RegExp('^import ' + impName('module') + '$', 'gm')),
+    transform: (j, item, match) => {}
+  },
+  binding: {
+    re: named(new RegExp('^import ' + impName('module') + ' as ' + impName('binding') + '$', 'gm')),
+    transform: (j, item, match) => {}
+  },
+  from: {
+    re: named(new RegExp('^from ' + impName('module') + ' import ' + impName('selection') + '$', 'gm')),
+    transform: (j, item, match) => {
+      const modulePath = match.captures.module[0];
+      const selection = match.captures.selection[0];
+
+      const importDec = j.importDeclaration(
+        [j.importSpecifier(j.identifier(selection), j.identifier(selection))],
+        j.literal(modulePath)
+      );
+
+      return j(item).replaceWith(importDec);
+    }
+  },
+  bindingFrom: {
+    re: named(new RegExp('^from ' + impName('module') + ' import ' + impName('selection') + ' as ' + impName('binding') + '$', 'gm')),
+    transform: (j, item, match) => {}
+  }
+};
+
+
+const transformImport = (j, item) => {
+  console.log('\n');
+  // console.log('item=', item);
+
+  if (item.value.arguments.length !== 1) {
+    throw new Error('Arguments length is not 1', item.value.arguments.length);
+  }
+
+  const argumentNode = item.value.arguments[0];
+  if (argumentNode.type !== 'Literal') {
+    throw new Error('Argument type not Literal', argumentNode.type);
+  }
+
+  const importString = argumentNode.value;
+  console.log('importString=', importString);
+
+  let match;
+  let importPattern;
+  _.forEach(importPatterns, (pattern, reName) => {
+    const newMatch = pattern.re.exec(importString);
+    if (newMatch) {
+      if (match) {
+        throw new Error('Ambiguous import match');
+      }
+      match = newMatch;
+      importPattern = pattern;
+    }
+  });
+
+  if (!match) {
+    throw new Error('Could not match import signature');
+  }
+
+  console.log('match=', match);
+  return importPattern.transform(j, item, match);
+};
+
+
+const toSourceOpts = { quote: 'single' };
 
 module.exports = function(fileInfo, api, options) {
   const j = api.jscodeshift;
@@ -26,30 +101,12 @@ module.exports = function(fileInfo, api, options) {
 
   shifted.find(j.CallExpression, { callee: { name: 'jsio' } })
     .forEach(item => {
-      // console.log('item=', item);
-
-      if (item.value.arguments.length !== 1) {
-        console.warn('Arguments length is not 1', item.value.arguments.length);
-        return;
+      try {
+        return transformImport(j, item);
+      } catch (err) {
+        console.warn('Error transforming import');
+        console.warn(err.stack);
       }
-
-      const argumentNode = item.value.arguments[0];
-      if (argumentNode.type !== 'Literal') {
-        console.warn('Argument type not Literal', argumentNode.type);
-        return;
-      }
-
-      const importString = argumentNode.value;
-      console.log('importString=', importString);
-
-      // process.exit(1)
-      // return j.identifier();
-      // const modulePath = jsio.__util.resolveModulePath();
-      const modulePath = 'asdf';
-
-      return j(item).replaceWith(
-        j.importDeclaration([], j.literal(modulePath))
-      );
     });
-  return shifted.toSource();
+  return shifted.toSource(toSourceOpts);
 };
--- oldCodeExamples/imports.js
+++ oldCodeExamples/imports.js
@@ -1,6 +1,35 @@
 // Should convert
-import event.Emitter as Emitter;
-import animate.transitions as transitions;
-import timer;
+// ---- ----
+
+// Single import
+import device;
+
+// Single relative import
+import .View;
+
+// Note: Single imports are imported with breadcrumb:
+// https://github.com/gameclosure/timestep/blob/664ac57cfede56923f8c2e181652a8f065059fff/src/platforms/browser/MobileBrowserAPI.js#L24
+// This means these should warn (no transform).  A separate mod will be needed
+// to convert all references to the full path
+import lib.PubSub;
+import ...ui.keyboardTypes;
+import .layout.BackingExtension;
+
+// Single import with rename
+import ui.View as View;
+import lib.PubSub as PubSub;
+import event.input.keys as keyConstants;
+
+// Selective imports
+from util.browser import $;
+
+// Import as export
+import Sound as exports;
+import ..CellView as exports;
+import .InputEvent as exports.InputEvent;
+
+
+
+// ---- ----
 // Should warn
 jsio('import ObjectPool', { supressErrors: true });
